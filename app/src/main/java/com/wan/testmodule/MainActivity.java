4c80217b18508b5c5bd3776d7e6102afc1ffe9c2
Richard-ZHOU1026 <871227655@qq.com>
2018-09-07 17:25:30 +0800

    需更新

---
+++ app/src/main/java/com/wan/testmodule/MainActivity.java
@@ -0,0 +1,20 @@
+package com.wan.testmodule;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wan.modulea.FirstActivtiy;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startActivity(new Intent(MainActivity.this, FirstActivtiy.class));
+        finish();
+    }
+
+
+}
---
+++ modulea/src/main/java/com/wan/modulea/FirstActivtiy.java
@@ -0,0 +1,13 @@
+package com.wan.modulea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstActivtiy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_activtiy);
+    }
+}
